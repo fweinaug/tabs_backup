161e5505ac0dd514c01529690d1fa7327085c145
Florian Weinaug <florianweinaug@outlook.de>
2020-12-06 15:59:05 +0100

    refactor html generation

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const fs = require('fs');
 const fileStream = fs.createReadStream('tabs.json');
 const jsonStream = StreamArray.withParser();
 
-const stream = fs.createWriteStream('tabs.html');
-
-stream.write('<!DOCTYPE NETSCAPE-Bookmark-file-1>\n');
-stream.write('<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n');
-stream.write('<TITLE>Tabs</TITLE>\n');
-stream.write('<H1>Tabs</H1>\n');
-stream.write('<DL>\n');
-stream.write('    <DT><H3>Tabs</H3></DT>\n');
-stream.write('    <DL>\n');
+const links = [];
 
 const processingStream = new Writable({
     write({key, value}, encoding, callback) {
@@ -21,7 +13,14 @@ const processingStream = new Writable({
         const url = value['url'];
 
         if (url && title) {
-            stream.write(`        <DT><A HREF="${url}">${title}</A></DT>\n`);
+            links.push({
+                type: 'dt',
+                content: {
+                    type: 'a',
+                    attributes: { href: url },
+                    content: title
+                }
+            });
         }
 
         callback();
@@ -33,7 +32,55 @@ fileStream.pipe(jsonStream.input);
 jsonStream.pipe(processingStream);
 
 processingStream.on('finish', () => {
-    stream.write('    </DL>\n');
-    stream.write('</DL>\n');
-    stream.close();
+    const elements = [
+        { content: '<!DOCTYPE NETSCAPE-Bookmark-file-1>' },
+        { content: '<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">' },
+        { type: 'title', content: 'Tabs' },
+        { type: 'h1', content: 'Tabs' },
+        {
+            type: 'dl',
+            content: [
+                {
+                    type: 'dt',
+                    content: { type: 'h3', content: 'Tabs' }
+                },
+                {
+                    type: 'dl',
+                    content: links
+                }
+            ]
+        }
+    ];
+
+    const html = elements.map(element => createHtmlElement(element)).join('\n');
+
+    fs.writeFile('tabs.html', html, fsErr => {});
 });
+
+const createHtmlElement = ({ type, attributes, content }, level = 0) => {
+    if (type) {
+        if (content.constructor === Object) {
+            return createHtmlElement(content, level);
+        }
+
+        const pad = ''.padStart(level * 4);
+        const tag = type.toUpperCase();
+
+        if (content.constructor === Array) {
+            const children = content.map(element => createHtmlElement(element, level + 1)).join('\n');
+
+            return `${pad}<${tag}>\n${children}\n${pad}</${tag}>`;
+        }
+
+        return `${pad}<${tag}${createHtmlAttributes(attributes)}>${content}</${tag}>`;
+    }
+
+    return content;
+}
+
+const createHtmlAttributes = (attributes) => {
+    if (attributes && attributes.constructor === Object) {
+        return Object.keys(attributes).map(key => ` ${key.toUpperCase()}="${attributes[key]}"`).join('');
+    }
+    return '';
+};
